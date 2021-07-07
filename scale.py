d30f35c09dac117b8b339a07131b4d45aa578119
Steve Cook <awscook@amazon.com>
2021-07-07 11:27:33 +1000

    Formatting changes

--- scale.py
+++ scale.py
@@ -5,8 +5,8 @@ glue = boto3.client("glue")
 kds = boto3.client("kinesis")
 
 
-# Find running glue jobs and stop them. 
-# This code assumes that there is only one running task per job definition. 
+# Find running glue jobs and stop them.
+# This code assumes that there is only one running task per job definition.
 def stop_running_jobs():
     # Find jobs that need to scale up/down, these jobs are tagged 'Scale' : 1
     res = glue.list_jobs(Tags={"Scale": "1"})
@@ -26,43 +26,45 @@ def stop_running_jobs():
 
         # Wait for job to stop
         print(f"Stopping job {running_job_ids[0]}")
-        status = ''
+        status = ""
         while status != "STOPPED":
             status = glue.get_job_run(JobName=job, RunId=running_job_ids[0])["JobRun"]["JobRunState"]
             print(status)
             time.sleep(1.5)
 
 
-# Start jobs back up
+# Start jobs back up with new number of workers.
 def start_jobs(num_workers):
     res = glue.list_jobs(Tags={"Scale": "1"})
     jobs = res["JobNames"]
 
     for job in jobs:
-        res = glue.start_job_run(JobName=job, NumberOfWorkers=num_workers, WorkerType='G.1X')
+        res = glue.start_job_run(JobName=job, NumberOfWorkers=num_workers, WorkerType="G.1X")
         print(f'Job Run Id {res["JobRunId"]}')
 
 
 # Updates Shard Count in Stream and Waits for it become Active
-# Note the restrictions on how the shard count can change 
+# Note the restrictions on how the shard count can change
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesis.html#Kinesis.Client.update_shard_count
 # For example to go from 1 shard to 8, this function would need to be called three times
 # udpate_shards('TestStream', 2)
 # udpate_shards('TestStream', 4)
 # udpate_shards('TestStream', 8)
 def update_shards(stream_name, target_shard_count):
-    res = kds.update_shard_count(StreamName=stream_name, TargetShardCount=target_shard_count, ScalingType='UNIFORM_SCALING')
-    assert(res['TargetShardCount'] == target_shard_count)
-    
-    status = ''
-    while status != 'ACTIVE':
+    res = kds.update_shard_count(
+        StreamName=stream_name, TargetShardCount=target_shard_count, ScalingType="UNIFORM_SCALING"
+    )
+    assert res["TargetShardCount"] == target_shard_count
+
+    status = ""
+    while status != "ACTIVE":
         time.sleep(1.5)
-        status = kds.describe_stream(StreamName=stream_name)['StreamDescription']['StreamStatus']
-        print(f'Stream {stream_name} is {status}')
+        status = kds.describe_stream(StreamName=stream_name)["StreamDescription"]["StreamStatus"]
+        print(f"Stream {stream_name} is {status}")
 
 
 # update_shards('TestStream', 2)
 
 # start_jobs()
 
-stop_running_jobs() 
+# stop_running_jobs()
